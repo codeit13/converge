56eabbbbffd45b952f75a20bd90b1b635b80c3a9
Sumit Chauhan <45000045+codeit13@users.noreply.github.com>
2025-08-24 07:48:31 +0530

    Update PROJECT_ID in constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -11,5 +11,5 @@ export const PAYMENTS_AUTH_URL = isLocalhost
   : "https://payments.sleebit.com/v1";
 
 export const PROJECT_ID = isLocalhost
-  ? "67e45b67cd2f9d001b6ec778"
-  : "67e45b67cd2f9d001b6ec778";
+  ? "68aa76141f3a2d001c93e576"
+  : "68aa76141f3a2d001c93e576";
